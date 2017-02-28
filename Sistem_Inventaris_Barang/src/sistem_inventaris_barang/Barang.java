d2f77546493a0385a4eb33f25173d7401304b8b8
nafasamuth <nafasa.muth@outlook.com>
2017-02-28 13:50:30 +0800

    yeah

---
+++ Sistem_Inventaris_Barang/src/sistem_inventaris_barang/Barang.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sistem_inventaris_barang;
+
+/**
+ *
+ * @author Farid Kiftirul Aziz
+ */
+public class Barang {
+    
+}
